3d119c53eb58848bc89c8200c7891427cad867dd
Steven <stevenpmcfarlane@gmail.com>
2021-03-08 16:45:06 -0700

    second commit

--- api/server.js
+++ api/server.js
@@ -9,4 +9,22 @@ server.use(express.json());
 
 //Endpoints
 
+server.get("/api/users/:id", (req, res) => {
+  //where is ID param in url coming from?
+  const id = req.params.id; //object that holds all variable objects in url
+  Users.findById(id)
+    .then((user) => {
+      if (user) {
+        res
+          .status(404)
+          .json({ message: "Dog not found with this id not found" });
+      } else {
+        res.json(user); //could put status 200, but that is default, so no necessary
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
